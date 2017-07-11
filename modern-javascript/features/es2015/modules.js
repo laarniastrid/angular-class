cf8aed1e7bd3b6bdff375b22aed26f048becc291
Astrid Sutton <astrid.sutton@wgu.edu>
2017-07-11 10:08:19 -0600

    spelling error

---
+++ modern-javascript/features/es2015/modules.js
@@ -0,0 +1,11 @@
+// require files
+import something from 'framework';
+import * as somethingf from 'framework';
+import {matchedProp} from 'framework';
+
+// expose values
+export default function something();
+export var value = 'value';
+export var another = 'avlue2';
+export var matchedProp = 'someValue';
+
